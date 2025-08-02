81a349b8c26f6f4d3f997ea9b9203af2d9b6ea79
John Kirschner <handfistface@gmail.com>
2025-08-02 15:15:17 -0400

    Enhance search modal in createCollection template with scrollable results and improved styling

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ app.register_blueprint(mealCollectionsBlueprint, url_prefix="/collections")
 app.register_blueprint(collection_ingredients_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
